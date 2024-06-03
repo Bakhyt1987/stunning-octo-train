58ca6f1ba420361b98fada7c55c66a776968650c
Bakhyt1987 <b-root@list.ru>
2024-06-03 16:13:06 +0500

    working wtih arrays

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-console.log('hello world')
-console.log('hello world')
-console.log('hello world')
-console.log('hello world')
+const tasks = ['task 1']
+
+function Add(task) {
+    tasks.push(task)
+}
+
+function Remove(task) {
+    const index = tasks.indexOf(task)
+    if (index === -1) {
+        return
+    }
+    tasks.splice(index, 1)
+
+}
+
+function Prioritize(task) {
+    const index = tasks.indexOf(task)
+    if (index === -1) {
+        return
+    }
+    const oldTask = tasks[index]
+    tasks.splice(index, 1)
+    tasks.unshift(oldTask)
+}
+
+Add('task 2')
+Add('task 3')
+//console.log(tasks)
+Remove('sdasd 2')
+console.log(tasks)
+
+Prioritize('task 3asdas')
+console.log(tasks)
